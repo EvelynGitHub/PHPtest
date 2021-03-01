0502f0d555ecc31b1c42d949915737714f6f536e
Evelyn <evelynbrandao15@gmail.com>
2021-03-01 14:42:54 -0300

    Variaveis de ambiente dev, prontas para usar com o Docker

---
+++ env.php
@@ -0,0 +1,16 @@
+<?php
+
+// Variável de ambiente URL
+putenv("URL=localhost");
+
+putenv("DB_DRIVE=mysql");
+
+putenv("DB_HOST=localhost");
+
+putenv("DB_PORT=3306");
+
+putenv("DB_NAME=phptest");
+
+putenv("DB_USER_NAME=root");
+
+putenv("DB_USER_PASSWD=toor");
