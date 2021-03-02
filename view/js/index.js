47ed85c96f849d7e04e2eca4ee6adc0bf23ca22b
Evelyn <evelynbrandao15@gmail.com>
2021-03-02 18:54:13 -0300

    Interface e funções concluidas

--- view/js/index.js
+++ view/js/index.js
@@ -1,36 +1,63 @@
 const urlBase = "http://localhost"
 
-console.log("Carreando js...")
+console.log("Carregando js...")
 
 let inputCep = document.getElementById("cep")
+let inputLocalidade = document.getElementById("localidade")
+let inputBairro = document.getElementById("bairro")
+let inputLogradouro = document.getElementById("logradouro")
+let inputEstado = document.getElementById("estado")
+let inputIbge = document.getElementById("ibge")
+let inputDDD = document.getElementById("ddd")
+let inputGIA = document.getElementById("gia")
+let inputSiafi = document.getElementById("siafi")
+let inputComplemento = document.getElementById("complemento")
 
-document.getElementById("search").addEventListener("click", function (e) {
+let messageArea = document.getElementById("message")
+
+let btnSearch = document.getElementById("search")
+
+
+btnSearch.addEventListener("click", function (e) {
     e.preventDefault();
-    console.log("Foi clicado")
 
-    console.log("CEP pesquisado: " + inputCep.value);
+    if (inputCep.value.trim() == "") {
+        message("danger", "o campo CEP não pode estar vazio.")
+        return
+    }
 
     // Varifica se existe esse cep na base de dados
-    let hasInBD = findCepInDB(inputCep.value);
+    let hasInBD = convertInObject(findCepInDB(inputCep.value));
 
-    console.log("Existe esse cep no BD: ", hasInBD)
+    console.log("Existe esse cep no BD: ", !hasInBD.search_api)
 
     // Se existir no banco trago os dados de lá
-    if (hasInBD) {
-        console.log("Dados do Banco");
-        console.log(hasInBD);
-    } else {//Se não existir no banco
+    if (!hasInBD.error && !hasInBD.search_api) {
+        console.log("Buscando no Banco de Dados");
+        message("primary", "Preenchendo os campos com os valores do Banco de Dados.")
+        fillInputs(hasInBD.msg)
+
+    } else if (hasInBD.error) {
+        message("danger", hasInBD.msg)
+        clearInputs();
+        return
+    } else {
+        //Se não existir no banco
         //busco na API 
-        let hasInAPI = findCepInViaCep(inputCep.value);
-        console.log(JSON.parse(hasInAPI));
+        let hasInAPI = (findCepInViaCep(inputCep.value));
 
-        if (hasInAPI) {
-            // salvo o resultado no banco
-            addInBD(JSON.parse(hasInAPI));
-
+        console.log("Deveria salvar no banco ?");
+        if (!("erro" in hasInAPI)) {
+            console.log("Sim, deveria, pois não tem erro");
+            addInBD(hasInAPI);
+            message("primary", "Preenchendo os campos com os valores do ViaCEP.")
+            fillInputs(hasInAPI);
         } else {
-            // caso não tenha valor na API
-            console.log("Não tem valor");
+            console.log("Não, não deveria, não foi encontrado o CEP na API");
+
+            message("warning", "Sinto muito, não encontramos o CEP que procura!");
+            clearInputs();
+            return
         }
 
 
@@ -44,7 +71,7 @@ function findCepInDB(cep) {
     ajax({
         method: "GET",
         url: urlBase + "/" + cep,
-        assinc: true,
+        assinc: false,
         data: null,
         dataType: null,
         success(e) {
@@ -66,13 +93,12 @@ function findCepInViaCep(cep) {
 
     ajax({
         method: "GET",
-        url: `http://viacep.com.br/ws/${cep}/json/`,
+        url: `http://viacep.com.br/ws/${cep}/xml/`,
         assinc: false,
         data: null,
         dataType: null,
         success(e) {
-            // console.log(e)
-            data = e;
+            data = convertInObject(e, true);
         },
         error(e) {
             console.log(e);
@@ -93,7 +119,7 @@ function addInBD(obj) {
         data: params,
         dataType: true,
         success(e) {
-            console.log(e)
+            console.log(e);
         },
         error(e) {
             console.log(e);
@@ -101,6 +127,55 @@ function addInBD(obj) {
     });
 
 }
+
+function convertInObject(params, api = false) {
+    if (api) {
+        return JSON.parse(xml2json(params));
+    }
+
+    return JSON.parse(params);
+}
+
+
+function message(cor, message) {
+    // alert(message)
+
+    messageArea.innerHTML = `
+        <div id="alert-message" class="alert alert-${cor} alert-dismissible fade show" role="alert">
+            ${message}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`;
+}
+
+function fillInputs(obj) {
+    clearInputs();
+    inputLocalidade.value = obj.localidade;
+    inputCep.value = obj.cep;
+    inputBairro.value = obj.bairro;
+    inputLogradouro.value = obj.logradouro;
+    inputEstado.value = obj.uf;
+    inputIbge.value = obj.ibge;
+    inputDDD.value = obj.ddd;
+    inputGIA.value = obj.gia;
+    inputSiafi.value = obj.siafi;
+    inputComplemento.value = obj.complemento;
+}
+
+function clearInputs() {
+    inputLocalidade.value = '';
+    inputCep.value = ''
+    inputBairro.value = ''
+    inputLogradouro.value = ''
+    inputEstado.value = ''
+    inputIbge.value = ''
+    inputDDD.value = ''
+    inputGIA.value = ''
+    inputSiafi.value = ''
+    inputComplemento.value = ''
+}
+
 
 function prepareData(data) {
     if (typeof data == 'string') return data;
@@ -149,3 +224,24 @@ function ajax(params) {
 
 }
 
+
+function xml2json(xmlDoc) {
+
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(xmlDoc, "text/xml");
+
+    let xml = xmlDoc.getElementsByTagName("xmlcep")[0];
+
+    let json = "{";
+
+    for (let i = 0; i < xml.children.length; i++) {
+        json += `"${xml.children[i].tagName}": "${xml.children[i].textContent}",`;
+    }
+
+    json = json.replace(/,\s*$/, "");
+
+    json += "}";
+
+
+    return (json);
+}
